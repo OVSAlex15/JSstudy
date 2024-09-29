abee6ca0271eeaf5156b530ef016029fb895030e
OVSAlex15 <103588591+OVSAlex15@users.noreply.github.com>
2024-09-29 19:09:31 +0600

    day 1
    day 1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert(2 != 1);
+// day 1 level 1
+let challenge = '30 days of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(0,6))
+console.log(challenge.includes("Script"))
+console.log(challenge.split())
+console.log(challenge.split(" "))
+
