28d42ac9724aaf9c6f1518d98cfeed784fdd32f3
OVSAlex15 <103588591+OVSAlex15@users.noreply.github.com>
2024-10-01 17:15:34 +0600

    day2 level 1
    day2 level 1

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// day 1 level 1
+// day 2 level 1
 let challenge = '30 days of JavaScript'
 console.log(challenge)
 console.log(challenge.length)
 console.log(challenge.toUpperCase())
 console.log(challenge.toLowerCase())
 console.log(challenge.substring(0,6))
+console.log(challenge.substring(3,21))
 console.log(challenge.includes("Script"))
 console.log(challenge.split())
 console.log(challenge.split(" "))
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(string.split(", "))
+console.log(challenge.replace("JavaScript", 'Python'))
+challenge.charAt(15)
+console.log(challenge.charCodeAt(challenge.length -1))
+console.log(challenge.indexOf("3"))
+console.log(challenge.trim(" "))
+console.log(challenge.startsWith("30"))
+console.log(challenge.endsWith("Script"))
+console.log(challenge.repeat(2))
+
 
